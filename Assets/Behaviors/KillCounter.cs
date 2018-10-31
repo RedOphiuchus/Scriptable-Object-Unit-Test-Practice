8cc2ab1c39e77cefcbd48066936c51ed2d55a376
Sera <005185709@coyote.csusb.edu>
2018-10-31 12:04:47 -0700

    Added Kill Counter

---
+++ Assets/Behaviors/KillCounter.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class KillCounter : MonoBehaviour {
+	public IntReference kills;
+	public void incrementKillCount()
+	{
+		++kills.Value;
+		transform.GetChild(1).GetComponent<Text>().text = kills.Value.ToString();
+	}
+
+	public void OnDestroy()
+	{
+		kills.Value = 0;
+	}
+}
