8a8e6183533b935156d034934c76d561e9ade201
Unknown <005705423@coyote.csusb.edu>
2018-11-27 17:01:13 -0800

    Updated Version

--- Assets/Behaviors/KillCounter.cs
+++ Assets/Behaviors/KillCounter.cs
@@ -11,16 +11,16 @@ public class KillCounter : MonoBehaviour {
 
 	public GameEvent Win;
 	public void incrementKillCount()
-	{
-		++kills.Value;
-		transform.GetChild(1).GetComponent<Text>().text = kills.Value.ToString();
-		if (kills.Value == goal)
-		{
-			Win.Raise();
-		}
-	}
+    {
+        ++kills.Value;
+        transform.GetChild(1).GetComponent<Text>().text = kills.Value.ToString();
+        if (kills.Value == goal)
+        {
+            Win.Raise();
+        }
+    }
 
-	public void OnDestroy()
+    public void OnDestroy()
 	{
 		kills.Value = 0;
 	}
